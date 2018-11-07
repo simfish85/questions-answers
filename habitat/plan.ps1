07300c6bd06368df30cd16a14832e655ca2849ef
Simon Fisher <simfish85@googlemail.com>
2018-11-07 14:59:59 +0000

    Added starter for MSBuild phase

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -3,6 +3,7 @@ $pkg_origin="simfish85"
 $pkg_version="0.1.0"
 $pkg_maintainer="Simon Fisher <simfish85@googlemail.com>"
 $pkg_license=@("Apache-2.0")
+$pkg_build_deps=@("core/visual-build-tools-2017", "core/nuget")
 
 function Invoke-Download {
     $Destination = "$HAB_CACHE_SRC_PATH/$pkg_name"
@@ -15,6 +16,11 @@ function Invoke-Download {
 
 function Invoke-Verify {
     return 0
+}
+
+function Invoke-Build {
+    $solutionPath = "$HAB_CACHE_SRC_PATH/$pkg_name/ModuleZeroSampleProject.sln"
+    MSBuild.exe $solutionPath /p:VisualStudioVersion=14.0 /p:PublishProfile=Release /p:DeployOnBuild=true
 }
 # $pkg_filename="$pkg_name-$pkg_version.zip"
 # $pkg_shasum="TODO"
