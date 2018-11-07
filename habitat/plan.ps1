efd4b1fbeee953d3b8c34d5efe3b2076b4b4b924
Simon Fisher <simfish85@googlemail.com>
2018-11-07 14:04:12 +0000

    Changed metadata to match app

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -1,9 +1,9 @@
-$pkg_name="habitat"
+$pkg_name="questions-answers"
 $pkg_origin="simfish85"
 $pkg_version="0.1.0"
-$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_maintainer="Simon Fisher <simfish85@googlemail.com>"
 $pkg_license=@("Apache-2.0")
-$pkg_source="http://some_source_url/releases/${pkg_name}-${pkg_version}.zip"
+$pkg_source="src"
 # $pkg_filename="$pkg_name-$pkg_version.zip"
 # $pkg_shasum="TODO"
 # $pkg_deps=@()
